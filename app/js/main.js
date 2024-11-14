85ea11e08855c943b5cfc53988d219ebb4acf25f
Nathank4242 <nkweon@gmail.com>
2024-11-14 13:59:43 -0500

    api

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,23 @@
 import "./style.css";
+async function getData() {
+    //return promsises
+    try{
+      const response = await fetch(`https://valorant-api.com/v1/agents`);
+      //gaurd clause
+      if (response.status !=200){
+          throw new Error(response);
+      }else
+      const data = await response.json
+      console.log(data.data.forEach(agent) => console.log(agent.displayName));
+      data.data forEach((agent) =>document.querySelector("div") .insertAdjacentHTML("afterbegin", `<h1>{agent.displayName}</h1>`))
+      ;
+
+
+    } catch (error){
+      alert("nuh uh")
+    }
+  
+  }
+  
+  getData();
+  

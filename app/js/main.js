da09785d9edcbfa7f22d8c9d25ad2f26d1fb44fa
Nathank4242 <nkweon@gmail.com>
2024-11-15 13:59:47 -0500

    api

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,11 @@
-import "./style.css";
 async function getData() {
-    //return promsises
-    try{
-      const response = await fetch(`https://valorant-api.com/v1/agents`);
-      //gaurd clause
-      if (response.status !=200){
-          throw new Error(response);
-      }else
-      const data = await response.json
-      console.log(data.data.forEach(agent) => console.log(agent.displayName));
-      data.data forEach((agent) =>document.querySelector("div") .insertAdjacentHTML("afterbegin", `<h1>{agent.displayName}</h1>`))
-      ;
-
-
-    } catch (error){
-      alert("nuh uh")
+  try {
+    const response = await fetch("https://random-d.uk/api");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
-  
-  }
-  
-  getData();
-  
+    const data = await response.json();
+    console.log(data);
+}}
+
+getData();

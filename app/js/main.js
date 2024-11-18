c513140ca8d38135c635d43c61912a23894a8e27
Nathank4242 <nkweon@gmail.com>
2024-11-18 14:01:42 -0500

    api

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,16 @@
 async function getData() {
   try {
-    const response = await fetch("https://random-d.uk/api");
+    const response = await fetch(
+      "https://botw-compendium.herokuapp.com/api/v3/compendium/all"
+    );
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
     const data = await response.json();
     console.log(data);
-}}
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+}
 
 getData();

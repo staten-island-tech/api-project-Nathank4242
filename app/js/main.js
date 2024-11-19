c3d6af056b78e63046d6990ba6db1d03ffc5294f
Nathank4242 <nkweon@gmail.com>
2024-11-19 14:01:19 -0500

    api

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
+let result = [];
 async function getData() {
   try {
     const response = await fetch(
@@ -6,11 +7,23 @@ async function getData() {
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
-    const data = await response.json();
-    console.log(data);
+    result = await response.json();
+    console.log(result);
   } catch (error) {
     console.error("There was a problem with the fetch operation:", error);
   }
 }
 
 getData();
+async function createCard() {
+  await getData();
+  result.data.forEach((thriller) => console.log(thriller.description));
+  Domselectors.container.insertAdjacentHTML(
+    "beforeend",
+    `
+    <h2>${thriller.description}</h2
+    <img scr="${thriller.image}><img>
+    `
+  );
+}
+createCard();
